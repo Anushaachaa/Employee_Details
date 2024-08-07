45b6fc5837085a6932769e9a016ca921764137be
Anusha GP <anushagp79@gmail.com>
2024-08-07 12:31:29 +0530

    Initial commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE clients(
+      id INT NOT NULL PRIMARY KEY IDENTITY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(150) NOT NULL UNIQUE,
+      phone VARCHAR(20) NULL,
+      address VARCHAR(100) NULL,
+      created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+      );
+
+INSERT INTO clients(name,email,phone,address)
+VALUES
+('Anusha','anushagp79@gmail.com','9019934566','udupi');
